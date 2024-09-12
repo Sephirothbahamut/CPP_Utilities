7053aaa334850c376dda051a3f5c8dca5c35c8dc
sephirothbahamut <sephirothbahamut@live.it>
2024-09-12 03:07:03 +0200

    cleared some code

--- include/utils/graphics/multisampling.h
+++ include/utils/graphics/multisampling.h
@@ -27,44 +27,37 @@ namespace utils::graphics
 		if constexpr (samples > 1)
 			{
 			constexpr float per_sample_delta{1.f / static_cast<float>(samples - 1)};
-			//*
-			sample_t sum;
-			
-			for (size_t y{0}; y < samples; y++)
-				{
-				for (size_t x{0}; x < samples; x++)
-					{
-					const utils::math::vec2f offset{x * per_sample_delta, y * per_sample_delta};
-					const utils::math::vec2f sample_coordinates{coordinates_f + offset};
-					
-					const auto sample{callback(sample_coordinates)};
-					sum += sample;
-					}
-				}
-			/*/
-			std::mutex sample_mutex;
-			sample_t sum;
 
-			std::ranges::iota_view indices(size_t{0}, samples * samples);
-			std::for_each(std::execution::par, indices.begin(), indices.end(), [&](size_t index)
-				{
-				size_t x{index / samples};
-				size_t y{index % samples};
+			sample_t sum{callback(coordinates_f)};
 
+			const auto inner_sample{[&](size_t x, size_t y) noexcept
+				{
 				const utils::math::vec2f offset{x * per_sample_delta, y * per_sample_delta};
 				const utils::math::vec2f sample_coordinates{coordinates_f + offset};
 
 				const auto sample{callback(sample_coordinates)};
-				std::unique_lock lock{sample_mutex};
-				sum += sample;
-				});
-			/**/
+				return sample;
+				}};
 
-			return sum / static_cast<float>(samples * samples);
+			for (size_t x{1}; x < samples; x++)
+				{
+				sum += inner_sample(x, 0);
+				}
+
+			for (size_t y{1}; y < samples; y++)
+				{
+				for (size_t x{0}; x < samples; x++)
+					{
+					sum += inner_sample(x, 0);
+					}
+				}
+
+			const auto ret{sum / static_cast<float>(samples * samples)};
+			return ret;
 			}
 		else 
 			{
-			sample_t ret{callback(coordinates_f)};
+			const sample_t ret{callback(coordinates_f)};
 			return ret;
 			}
 		}
