fc08e188bb019582568b90353a65f973915f960f
sephirothbahamut <sephirothbahamut@live.it>
2025-05-14 02:05:03 +0200

    reworked 2d matrix for_each

--- include/utils/logging/progress_bar.h
+++ include/utils/logging/progress_bar.h
@@ -15,33 +15,23 @@
 
 #include "../oop/disable_move_copy.h"
 
+//TODO remove inlines and split into .cpp
+
 namespace utils::logging
 	{
+	class partial_progress;
+
 	class progress_bar : utils::oop::non_copyable, utils::oop::non_movable
 		{
 		public:
-			progress_bar() = default;
-			progress_bar(float update_step, size_t bar_width) : update_step{update_step}, bar_width{bar_width} {}
+			progress_bar();
+			progress_bar(float update_step, size_t bar_width);
 
-			void advance(float new_state) noexcept
-				{
-				std::unique_lock lock{mutex};
-				if (new_state <= state) { return; }
-				state = new_state;
+			void advance(float new_state) noexcept;
 
-				const bool should_draw{(state == 1.f) || ((state - last_drawn_state) >= update_step)};
-				if (!should_draw) { return; }
+			void complete() noexcept;
 
-				last_drawn_state = state;
-				message_queue.emplace(state);
-				}
-
-			void complete() noexcept
-				{
-				advance(1.f);
-				message_queue.flush();
-				std::cout << std::endl;
-				}
+			partial_progress partial_progress(size_t steps_count) noexcept;
 
 		private:
 			float state{0.f};
@@ -58,49 +48,32 @@ namespace utils::logging
 					}
 				};
 
-			void draw(const float& percent) const noexcept
-				{
-				const size_t text_start_index{static_cast<size_t>(static_cast<float>(bar_width) / 2.f) - 2};
-				const std::string percent_string{[&percent]()
-					{
-					auto ret{std::to_string(static_cast<size_t>(std::round(percent * 100.f))) + "%"};
-					while (ret.size() < 4) { ret = ' ' + ret; }
-					return ret;
-					}()};
+			void draw(const float& percent) const noexcept;
+		};
 
-				const size_t bar_current_index{static_cast<size_t>(static_cast<float>(bar_width) * percent)};
+	class partial_progress : utils::oop::non_copyable, utils::oop::non_movable
+		{
+		friend class progress_bar;
+		public:
 
-				std::stringstream ss;
-				ss << '\r';
-				ss << utils::console::colour::restore_defaults << "[";
-				for (size_t i{0}; i < bar_width; i++)
-					{
-					if (i < bar_current_index)
-						{
-						const uint8_t r{static_cast<uint8_t>(std::pow((1.f - percent), 1.f/2.2f) * 127.f)};
-						const uint8_t g{static_cast<uint8_t>(std::pow(       percent , 1.f/2.2f) * 127.f)};
-						//const uint8_t r{static_cast<uint8_t>((1.f - percent) * 127.f)};
-						//const uint8_t g{static_cast<uint8_t>(       percent  * 127.f)};
+			void advance() noexcept;
 
-						ss << utils::console::colour::background{utils::graphics::colour::rgb_u{r, g, uint8_t{0}}};
-						}
-					else if(i == bar_current_index)
-						{
-						ss << utils::console::colour::background{utils::graphics::colour::rgb_u{uint8_t{50}, uint8_t{50}, uint8_t{50}}};
-						}
+			partial_progress split_partial_progress(size_t steps_count) noexcept;
 
-					if ((i >= text_start_index) && (i < (text_start_index + 4)))
-						{
-						ss << percent_string[i - text_start_index];
-						}
-					else
-						{
-						ss << ' ';
-						}
-					}
-				ss << utils::console::colour::restore_defaults << "]";
-				if (percent == 1.f) { ss << "\n\n"; }
-				std::cout << ss.str() << std::flush;
-				}
+		private:
+			partial_progress(progress_bar& progress_bar, float from, float to, size_t steps_count) noexcept;
+
+			std::mutex mutex;
+			progress_bar& progress_bar;
+			const float from{0.f};
+			const float to  {1.f};
+			const float step{0.f};
+			size_t index{0};
+
+			static float evaluate_step(float from, float to, size_t steps_count) noexcept;
 		};
 	}
+
+#ifdef utils_implementation
+#include "progress_bar.cpp"
+#endif
