949909f4984e7778adfdc9e2f6ff32df1e9cbf50
sephirothbahamut <sephirothbahamut@live.it>
2025-10-27 00:08:41 +0100

    Added explicit casts to constants in easings to silence warnings

--- include/utils/math/easings.h
+++ include/utils/math/easings.h
@@ -15,19 +15,19 @@ namespace utils::math::easing
 		template <std::floating_point T>
 		T ease_in_sine(T t)
 			{
-			return std::sin(1.5707963 * t);
+			return std::sin(static_cast<T>(1.5707963) * t);
 			}
 
 		template <std::floating_point T>
 		T ease_out_sine(T t)
 			{
-			return 1 + std::sin(1.5707963 * (--t));
+			return static_cast<T>(1) + std::sin(static_cast<T>(1.5707963) * (--t));
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_sine(T t)
 			{
-			return 0.5 * (1 + std::sin(3.1415926 * (t - 0.5)));
+			return static_cast<T>(0.5) * (static_cast<T>(1) + std::sin(static_cast<T>(3.1415926) * (t - static_cast<T>(0.5))));
 			}
 
 		template <std::floating_point T>
@@ -39,13 +39,13 @@ namespace utils::math::easing
 		template <std::floating_point T>
 		T ease_out_quad(T t)
 			{
-			return t * (2 - t);
+			return t * (static_cast<T>(2) - t);
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_quad(T t)
 			{
-			return t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1;
+			return t < static_cast<T>(0.5) ? static_cast<T>(2) * t * t : t * (static_cast<T>(4) - static_cast<T>(2) * t) - static_cast<T>(1);
 			}
 
 		template <std::floating_point T>
@@ -57,13 +57,13 @@ namespace utils::math::easing
 		template <std::floating_point T>
 		T ease_out_cubic(T t)
 			{
-			return 1 + (--t) * t * t;
+			return static_cast<T>(1) + (--t) * t * t;
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_cubic(T t)
 			{
-			return t < 0.5 ? 4 * t * t * t : 1 + (--t) * (2 * (--t)) * (2 * t);
+			return t < static_cast<T>(0.5) ? static_cast<T>(4) * t * t * t : static_cast<T>(1) + (--t) * (static_cast<T>(2) * (--t)) * (static_cast<T>(2) * t);
 			}
 
 		template <std::floating_point T>
@@ -77,21 +77,21 @@ namespace utils::math::easing
 		T ease_out_quart(T t)
 			{
 			t = (--t) * t;
-			return 1 - t * t;
+			return static_cast<T>(1) - t * t;
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_quart(T t)
 			{
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
 				t *= t;
-				return 8 * t * t;
+				return static_cast<T>(8) * t * t;
 				}
 			else
 				{
 				t = (--t) * t;
-				return 1 - 8 * t * t;
+				return static_cast<T>(1) - static_cast<T>(8) * t * t;
 				}
 			}
 
@@ -113,47 +113,47 @@ namespace utils::math::easing
 		T ease_in_out_quint(T t)
 			{
 			T t2;
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
 				t2 = t * t;
-				return 16 * t * t2 * t2;
+				return static_cast<T>(16) * t * t2 * t2;
 				}
 			else
 				{
 				t2 = (--t) * t;
-				return 1 + 16 * t * t2 * t2;
+				return static_cast<T>(1) + static_cast<T>(16) * t * t2 * t2;
 				}
 			}
 
 		template <std::floating_point T>
 		T ease_in_expo(T t)
 			{
-			return (std::pow(2, 8 * t) - 1) / 255;
+			return (std::pow(static_cast<T>(2), static_cast<T>(8) * t) - static_cast<T>(1)) / static_cast<T>(255);
 			}
 
 		template <std::floating_point T>
 		T ease_out_expo(T t)
 			{
-			return 1 - std::pow(2, -8 * t);
+			return static_cast<T>(1) - std::pow(static_cast<T>(2), -static_cast<T>(8) * t);
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_expo(T t)
 			{
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
-				return (std::pow(2, 16 * t) - 1) / 510;
+				return (std::pow(static_cast<T>(2), static_cast<T>(16) * t) - static_cast<T>(1)) / static_cast<T>(510);
 				}
 			else
 				{
-				return 1 - 0.5 * std::pow(2, -16 * (t - 0.5));
+				return static_cast<T>(1) - static_cast<T>(0.5) * std::pow(static_cast<T>(2), -static_cast<T>(16) * (t - static_cast<T>(0.5)));
 				}
 			}
 
 		template <std::floating_point T>
 		T ease_in_circ(T t)
 			{
-			return 1 - std::sqrt(1 - t);
+			return static_cast<T>(1) - std::sqrt(static_cast<T>(1) - t);
 			}
 
 		template <std::floating_point T>
@@ -165,38 +165,38 @@ namespace utils::math::easing
 		template <std::floating_point T>
 		T ease_in_out_circ(T t)
 			{
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
-				return (1 - std::sqrt(1 - 2 * t)) * 0.5;
+				return (static_cast<T>(1) - std::sqrt(static_cast<T>(1) - static_cast<T>(2) * t)) * static_cast<T>(0.5);
 				}
 			else
 				{
-				return (1 + std::sqrt(2 * t - 1)) * 0.5;
+				return (static_cast<T>(1) + std::sqrt(static_cast<T>(2) * t - static_cast<T>(1))) * static_cast<T>(0.5);
 				}
 			}
 
 		template <std::floating_point T>
 		T ease_in_back(T t)
 			{
-			return t * t * (2.70158 * t - 1.70158);
+			return t * t * (static_cast<T>(2.70158) * t - static_cast<T>(1.70158));
 			}
 
 		template <std::floating_point T>
 		T ease_out_back(T t)
 			{
-			return 1 + (--t) * t * (2.70158 * t + 1.70158);
+			return static_cast<T>(1) + (--t) * t * (static_cast<T>(2.70158) * t + static_cast<T>(1.70158));
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_back(T t)
 			{
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
-				return t * t * (7 * t - 2.5) * 2;
+				return t * t * (static_cast<T>(7) * t - static_cast<T>(2.5)) * static_cast<T>(2);
 				}
 			else
 				{
-				return 1 + (--t) * t * 2 * (7 * t + 2.5);
+				return static_cast<T>(1) + (--t) * t * static_cast<T>(2) * (static_cast<T>(7) * t + static_cast<T>(2.5));
 				}
 			}
 
@@ -204,58 +204,58 @@ namespace utils::math::easing
 		T ease_in_elastic(T t)
 			{
 			T t2 = t * t;
-			return t2 * t2 * std::sin(t * std::numbers::pi * 4.5);
+			return t2 * t2 * std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(4.5));
 			}
 
 		template <std::floating_point T>
 		T ease_out_elastic(T t)
 			{
-			T t2 = (t - 1) * (t - 1);
-			return 1 - t2 * t2 * std::cos(t * std::numbers::pi * 4.5);
+			T t2 = (t - static_cast<T>(1)) * (t - static_cast<T>(1));
+			return static_cast<T>(1) - t2 * t2 * std::cos(t * static_cast<T>(std::numbers::pi) * static_cast<T>(4.5));
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_elastic(T t)
 			{
 			T t2;
-			if (t < 0.45)
+			if (t < static_cast<T>(0.45))
 				{
 				t2 = t * t;
-				return 8 * t2 * t2 * std::sin(t * std::numbers::pi * 9);
+				return static_cast<T>(8) * t2 * t2 * std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(9));
 				}
-			else if (t < 0.55)
+			else if (t < static_cast<T>(0.55))
 				{
-				return 0.5 + 0.75 * std::sin(t * std::numbers::pi * 4);
+				return static_cast<T>(0.5) + static_cast<T>(0.75) * std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(4));
 				}
 			else
 				{
-				t2 = (t - 1) * (t - 1);
-				return 1 - 8 * t2 * t2 * std::sin(t * std::numbers::pi * 9);
+				t2 = (t - static_cast<T>(1)) * (t - static_cast<T>(1));
+				return static_cast<T>(1) - static_cast<T>(8) * t2 * t2 * std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(9));
 				}
 			}
 
 		template <std::floating_point T>
 		T ease_in_bounce(T t)
 			{
-			return std::pow(2, 6 * (t - 1)) * std::abs(std::sin(t * std::numbers::pi * 3.5));
+			return std::pow(static_cast<T>(2), static_cast<T>(6) * (t - static_cast<T>(1))) * std::abs(std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(3.5)));
 			}
 
 		template <std::floating_point T>
 		T ease_out_bounce(T t)
 			{
-			return 1 - std::pow(2, -6 * t) * std::abs(std::cos(t * std::numbers::pi * 3.5));
+			return static_cast<T>(1) - std::pow(static_cast<T>(2), -static_cast<T>(6) * t) * std::abs(std::cos(t * static_cast<T>(std::numbers::pi) * static_cast<T>(3.5)));
 			}
 
 		template <std::floating_point T>
 		T ease_in_out_bounce(T t)
 			{
-			if (t < 0.5)
+			if (t < static_cast<T>(0.5))
 				{
-				return 8 * std::pow(2, 8 * (t - 1)) * std::abs(std::sin(t * std::numbers::pi * 7));
+				return static_cast<T>(8) * std::pow(static_cast<T>(2), static_cast<T>(8) * (t - static_cast<T>(1))) * std::abs(std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(7)));
 				}
 			else
 				{
-				return 1 - 8 * std::pow(2, -8 * t) * std::abs(std::sin(t * std::numbers::pi * 7));
+				return static_cast<T>(1) - static_cast<T>(8) * std::pow(static_cast<T>(2), -static_cast<T>(8) * t) * std::abs(std::sin(t * static_cast<T>(std::numbers::pi) * static_cast<T>(7)));
 				}
 			}
 		}
@@ -299,6 +299,7 @@ namespace utils::math::easing
 			case utils::math::easing::bounce:  if constexpr (dir == dir::in)     { return _::ease_in_bounce     (t); }
 			                              else if constexpr (dir == dir::out)    { return _::ease_out_bounce    (t); }
 			                              else if constexpr (dir == dir::in_out) { return _::ease_in_out_bounce (t); }
+			default: std::unreachable();
 			}
 		}
 
